cf1b155c0b2a7399c4970c8a4ec4b2693d3a0fe0
inesaaaa <inesa.afitsaryan@jetbrains.com>
2025-02-25 17:36:52 +0400

    Update Dumb.java

--- src/main/java/com/flawedbank/Dumb.java
+++ src/main/java/com/flawedbank/Dumb.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.HashSet;
-import java.util.Collections;
+//import java.util.Collections;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
