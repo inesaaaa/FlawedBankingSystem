0cffb1abb8717c0ad8ca58586d9e0d9662a8e200
Inesa Afitsaryan <inesa.afitsaryan@jetbrains.com>
2026-08-28 15:05:42 +0400

    add all

--- src/main/java/com/flawedbank/Dumb.java
+++ src/main/java/com/flawedbank/Dumb.java
@@ -8,10 +8,10 @@ import java.util.HashMap;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Collections;
+//import java.util.Collections;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+//import java.lang.reflect.Method;
 
 public class Dumb {
     int a;
